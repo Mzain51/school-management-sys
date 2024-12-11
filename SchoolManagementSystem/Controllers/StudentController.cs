8231f4820fbb660dcf006af2ebab4f32c4339b76
Mzain51 <zainu03458@gmail.com>
2024-12-11 15:01:04 +0500

    initial commit

---
+++ SchoolManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagementSystem.Data;
+using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Students> students = _context.Students;
+            return View(students);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Students student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+             
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var student = _context.Students.Find(id);
+            return View(student);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Students student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Update(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Delete(int student)
+        {
+               var std = _context.Students.Find(student);
+                _context.Students.Remove(std);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            
+        }
+    }
+}
---
+++ SchoolManagementSystem/Models/Students.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolManagementSystem.Models
+{
+    public class Students
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public int Marks { get; set; }
+        public float Percentage { get; set; }
+
+    }
+}
